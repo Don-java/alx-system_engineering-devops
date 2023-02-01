970b89206ee43f9a600169be6c45185dc2dee04b
Don-Java <donijilad@gmail.com>
2023-01-31 22:20:59 -0800

    Regular expression

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/hbt+n/).join#!/usr/bin/env ruby
+puts ARGV[0].scan(/hbt+n/).join#!/usr/bin/env ruby
+puts ARGV[0].scan(/hbt+n/).join#!/usr/bin/env ruby
+puts ARGV[0].scan(/hbt+n/).join#!/usr/bin/env ruby
+puts ARGV[0].scan(/hbt+n/).join#!/usr/bin/env ruby
+puts ARGV[0].scan(/hbt+n/).join#!/usr/bin/env ruby
+puts ARGV[0].scan(/hbt+n/).join#!/usr/bin/env ruby
+puts ARGV[0].scan(/hbt+n/).join#!/usr/bin/env ruby
+puts ARGV[0].scan(/hbt+n/).join
