8b67a56e5787ad7996393d6b372e40b02b0218cf
Don-Java <donijilad@gmail.com>
2023-04-19 23:16:28 -0700

    Correction

--- 0x14-mysql/test.sql
+++ 0x14-mysql/test.sql
@@ -1,4 +1,4 @@
--- task 1
+ -- task 1
 CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
 GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
 
